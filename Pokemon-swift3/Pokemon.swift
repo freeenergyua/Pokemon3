e06cc4ba6e4ce1c04dbe06a819c72ea7a681d1fb
Andre <freeenergy@ukr.net>
2016-11-17 11:58:10 +0200

    design UI

---
+++ Pokemon-swift3/Pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  Pokemon.swift
+//  Pokemon-swift3
+//
+//  Created by User on 16.11.16.
+//  Copyright © 2016 AG. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+   
+    private var _name: String!
+    private var _pokemonId: Int!
+    
+    var name: String {
+        return _name
+    }
+    var pokemonId: Int {
+        return _pokemonId
+    }
+    
+    init(name:String, pokemonId: Int) {
+        self._name = name
+        self._pokemonId = pokemonId
+    }
+    
+}
